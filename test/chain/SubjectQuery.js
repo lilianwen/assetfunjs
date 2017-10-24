aa5e7f8758c435d9bee0700c74ab6ccfd7a05b2d
llw <496852470@qq.com>
2017-10-24 09:45:51 +0800

    AddFile:添加源代码，方便查看

---
+++ test/chain/SubjectQuery.js
@@ -0,0 +1,112 @@
+import assert from "assert";
+//import SubjectQuery from "../../lib";
+import {Apis, ChainConfig} from "fidchainjs-ws";
+//import {GetSubjectsByName} from "../../lib/chain/src/SubjectQuery"
+import {GetSubjectById, GetSubjectsByName, GetSubjectsOrderById, 
+    GetSubjectsByStatus, GetSubjectsByCreator, GetSubjectsByCreateTime, 
+    GetSubjectsByVoteEndTime, GetSubjectVotesByVoter, GetSubjectVotesBySubjectId, 
+    GetSubjectEventsBySubjectId, GetSubjectEventsByOperator} from "../../lib"
+
+describe("SubjectQuery functions test", () => {
+
+	let cs = "ws://106.15.202.130:11011";
+	before(function() {
+            return Apis.instance(cs, true).init_promise.then(function (result) {
+                console.log("conneted to ws://106.15.202.130:11011")
+            });
+        });
+
+	it("Get subjects by id", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectById("1.16.0").then(function(subject){
+           		console.log(subject);
+                if (subject) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject whose id is 1.16.1."))
+           		}	
+           })
+        })
+    })
+
+	it("Get subjects by name", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectsByName(1,10,"bitcoin_up_20006").then(function(subjects){
+           		console.log(subjects);
+                if (subjects) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject named bitcoin_up_20006."))
+           		}	
+           })
+        })
+    })
+
+    
+    it("Get subjects order by id", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectsOrderById(1,10).then(function(subjects){
+           		console.log(subjects);
+                if (subjects) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject named bitcoin_up_20006."))
+           		}	
+           })
+        })
+    })
+    
+    it("Get subjects by status", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectsByStatus(1,10,"create_status").then(function(subjects){
+           		console.log(subjects);
+                if (subjects) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject named bitcoin_up_20006."))
+           		}	
+           })
+        })
+    })
+
+    it("Get subjects by creator", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectsByCreator(1,10,"1.2.19").then(function(subjects){
+           		console.log(subjects);
+                if (subjects) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject named bitcoin_up_20006."))
+           		}	
+           })
+        })
+    })
+
+    it("Get subjects by creat time", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectsByCreateTime(1,10,"2017-10-20T07:14:57","2017-10-21T07:14:57").then(function(subjects){
+           		console.log(subjects);
+                if (subjects) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject named bitcoin_up_20006."))
+           		}	
+           })
+        })
+    })
+
+    it("Get subjects by vote end time", function(){
+        return new Promise(function(resolve, reject){
+            GetSubjectsByVoteEndTime(1,10,"2018-01-13T19:46:04","2019-01-13T19:46:04").then(function(subjects){
+           		console.log(subjects);
+                if (subjects) {
+           		    resolve()
+                }else{
+           			reject(Error("there is no subject named bitcoin_up_20006."))
+           		}	
+           })
+        })
+    })
+
+    
+})
