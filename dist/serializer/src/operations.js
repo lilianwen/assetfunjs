93f26ad2babf028afd7553980065112ad46bef9b
llw <496852470@qq.com>
2017-11-13 17:40:41 +0800

    FixProj:lib\serializer\src\operations.js内subject_vote末尾增加vote_time: time_point_sec字段

--- dist/serializer/src/operations.js
+++ dist/serializer/src/operations.js
@@ -720,7 +720,8 @@ var subject_vote = exports.subject_vote = new Serializer("subject_vote", {
     fee: asset,
     voter: protocol_id_type("account"),
     subject_id: protocol_id_type("subject"),
-    template_vote: subject_vote_template
+    template_vote: subject_vote_template,
+    vote_time: time_point_sec
 });
 
 var subject_event = exports.subject_event = new Serializer("subject_event", {
@@ -739,7 +740,7 @@ var coin_feed_price = exports.coin_feed_price = new Serializer("coin_feed_price"
     extensions: set(future_extensions)
 });
 
-operation.st_operations = [transfer, limit_order_create, limit_order_cancel, call_order_update, fill_order, account_create, account_update, account_whitelist, account_upgrade, account_transfer, asset_create, asset_update, asset_update_bitasset, asset_update_feed_producers, asset_issue, asset_reserve, asset_fund_fee_pool, asset_settle, asset_global_settle, asset_publish_feed, witness_create, witness_update, proposal_create, proposal_update, proposal_delete, withdraw_permission_create, withdraw_permission_update, withdraw_permission_claim, withdraw_permission_delete, committee_member_create, committee_member_update, committee_member_update_global_parameters, vesting_balance_create, vesting_balance_withdraw, worker_create, custom, assert, balance_claim, override_transfer, transfer_to_blind, blind_transfer, transfer_from_blind, asset_settle_cancel, asset_claim_fees, fba_distribute, subject_publish, subject_vote, subject_event, coin_feed_price, module_cfg];
+operation.st_operations = [transfer, limit_order_create, limit_order_cancel, call_order_update, fill_order, account_create, account_update, account_whitelist, account_upgrade, account_transfer, asset_create, asset_update, asset_update_bitasset, asset_update_feed_producers, asset_issue, asset_reserve, asset_fund_fee_pool, asset_settle, asset_global_settle, asset_publish_feed, witness_create, witness_update, proposal_create, proposal_update, proposal_delete, withdraw_permission_create, withdraw_permission_update, withdraw_permission_claim, withdraw_permission_delete, committee_member_create, committee_member_update, committee_member_update_global_parameters, vesting_balance_create, vesting_balance_withdraw, worker_create, custom, assert, balance_claim, override_transfer, transfer_to_blind, blind_transfer, transfer_from_blind, asset_settle_cancel, asset_claim_fees, fba_distribute, subject_publish, subject_vote, subject_event, coin_feed_price];
 
 var transaction = exports.transaction = new Serializer("transaction", { ref_block_num: uint16,
     ref_block_prefix: uint32,
